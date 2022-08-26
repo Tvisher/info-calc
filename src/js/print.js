1e1d21d5b7897b41bebecf0e33f0810a9aa96e49
Tvisher <tvisher@mail.ru>
2022-08-26 15:16:44 +0600

    css fix

--- src/js/print.js
+++ src/js/print.js
@@ -2,13 +2,14 @@ import sum_letters from './numTostring.js';
 
 export default function callPrint(data) {
     var windowPrint = window.open('', '', 'left=50,top=50,width=2000,height=2000,toolbar=0,scrollbars=1,status=0');
+
     const dataList = data.fullSettlementList;
-    const tableRows = dataList.reduce((row, item) => {
+    const tableRows = dataList.reduce((row, dataItem) => {
         const { metalType,
             metalThickness,
             cuttingLength,
             punchingCount,
-            itemPrice, } = item;
+            itemPrice, } = dataItem;
 
         const calculationItem = `
         <tr>
@@ -21,7 +22,7 @@ export default function callPrint(data) {
         return row;
     }, '');
     const clientName = data.clientName;
-    const clienText = clientName.length > 0 ? `для клиента:<strong style="display:inline">«${clientName}»</strong>` : '';
+    const clienText = clientName.length > 0 ? `для клиента:&nbsp<strong style="display:inline">«${clientName}»</strong>` : '';
 
     const finishResult = dataList.reduce((sum, current) => sum + current.itemPrice, 0);
     const finishResultInWords = sum_letters(finishResult);
@@ -29,38 +30,21 @@ export default function callPrint(data) {
     <div class="print-wrapper" style="max-width: 1024px; width: 100%; margin:auto">
         <div class="print-wrapper__head">
             <div class="comp-data">
-                <span> БИН 010440000789</span>
-                <span> ИИК KZ686010151000063517</span>
-                <span> ВКО Филиал АО «Народный Банк Казахстана»</span>
-                <span> г. Усть-Каменогорск</span>
-                <span> БИК HSBKKZKX</span>
-                <span> Св-во по НДС 18001 № 0022849 от 17.09.12 г.</span>
-                <span> 070018, Казахстан, Восточно-Казахстанская область,</span>
-                <span> Усть-Каменогорск, улица Бажова, 59</span>
-                <span> ИИН 999999999999</span>
+                <span> ИП «Шляхова А.Н.»</span>
+                <span> ИИН 780402302789</span>
+                <span> ул. Бажова, 95</span>
             </div>
             <div class="comp-logo">
                 <img src="./img/printLogo.png"" alt="">
             </div>
             <div class="comp-data r">
-                <span> БСН 010440000789</span>
-                <span> ИИК KZ686010151000063517</span>
-                <span> «Қазақстан Халық Банкі» АҚ Шығыс Қазақстан филиалы</span>
-                <span> Өскемен қаласы</span>
-                <span> BIC HSBKKZKX</span>
-                <span> ҚҚС 18001 № 0022849 куәлігі 17.09.12 ж.</span>
-                <span> 070018, Қазақстан, Шығыс Қазақстан облысы,</span>
-                <span> Өскемен қаласы, Бажов көшесі, 59</span>
-                <span> ЖСН 999999999999</span>
+                <span> 8 7232 700 350</span>
+                <span> 8 705 155 99 11</span>
+                <span> www.infoplus.kz</span>
             </div>
         </div>
         <div class="print-wrapper__body">
-            <span class="title">Коммерческое предложение от ${new Date().toLocaleDateString('ru-RU')}</span>
-            <span class="descr">на лазерную резку металла ${clienText}</span>
-            <!--  <span class="info">
-                Индивидуальный предприниматель Шляхов А. Н. направляет Вам на рассмотрение коммерческое предложение на лазерную резку металла, приведенное в таблице:
-            </span>-->
-
+            <span class="title">Расчёт от ${new Date().toLocaleDateString('ru-RU')} а лазерную резку металла ${clienText}</span>
             <table class="res-table">
                 <thead>
                     <tr>
@@ -77,10 +61,11 @@ export default function callPrint(data) {
             <div class="result-text">
                 Общая стоимость: <strong  style="display:inline;">${finishResult.toLocaleString('ru-RU')}</strong> (${finishResultInWords}) тенге.
             </div>
+            <div class="print-wrapper__head" style="margin-top:60px">
+                <div class="comp-data">Шляхов А. Н.  __________</div>
+                <div class="comp-data" style="padding-right:60px">М.П.</div>
+            </div>
         </div>
-        <style>
-
-        </style>
     </div>`;
     windowPrint.document.write(`<link rel="stylesheet" href="./css/print.css" type="text/css">`);
     windowPrint.document.write(printPage);
